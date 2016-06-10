c8904ab46e401250c4594b8fffb950cd2618adf0
jvenerosy <julien.venerosy@gmail.com>
2016-06-10 17:17:34 +0200

    deuxieme section ok

---
+++ 2_scope/js/controller.js
@@ -0,0 +1,54 @@
+var CommentsCtrl = function($scope) {
+
+  $scope.comments = [
+    {
+      "username": "Hewitt",
+      "city": "Levant",
+      "email": "<TypeError: undefined is not a function>",
+      "content": "Ad velit sint esse quis incididunt quis quis labore magna consequat qui fugiat eiusmod. Aute in reprehenderit sint dolore magna consectetur do ea ipsum ullamco sunt. Mollit mollit velit est eiusmod sit culpa ea eiusmod laborum nostrud id. Veniam pariatur excepteur aliqua magna. Adipisicing excepteur officia dolore sint minim non occaecat dolore."
+    },
+    {
+      "username": "Trina",
+      "city": "Warren",
+      "email": "<TypeError: undefined is not a function>",
+      "content": "Excepteur irure irure cillum ex occaecat nulla pariatur nostrud. Sunt dolore commodo consectetur dolor. Pariatur elit enim voluptate eu et ad ipsum in. Voluptate elit laborum excepteur ea eu. Adipisicing commodo culpa adipisicing labore ullamco dolor sunt nulla nisi amet ipsum proident incididunt velit."
+    },
+    {
+      "username": "Livingston",
+      "city": "Jugtown",
+      "email": "<TypeError: undefined is not a function>",
+      "content": "Irure consectetur commodo elit est cupidatat est consectetur cillum excepteur aliqua eiusmod. Quis aute sit eiusmod aliquip aliquip. Est aute veniam consequat eiusmod adipisicing Lorem amet consequat tempor magna qui. Sunt minim proident sint voluptate enim occaecat aliqua irure in. Officia occaecat ad laborum proident."
+    },
+    {
+      "username": "Regina",
+      "city": "Woodlands",
+      "email": "<TypeError: undefined is not a function>",
+      "content": "Commodo occaecat quis sint laborum. Voluptate aliqua dolor aliquip incididunt ullamco enim. Reprehenderit elit dolore veniam est labore ipsum minim non ut anim. Cupidatat dolore reprehenderit ut dolor anim dolor duis. Ad cillum consectetur dolore dolore in proident elit."
+    },
+    {
+      "username": "Essie",
+      "city": "Maury",
+      "email": "<TypeError: undefined is not a function>",
+      "content": "Id ex consequat reprehenderit fugiat magna voluptate consectetur commodo cupidatat adipisicing eiusmod aliquip non. Laboris magna velit pariatur culpa pariatur ipsum cupidatat culpa consequat do nulla qui. Anim nisi minim est est. Veniam velit minim reprehenderit mollit nisi dolore culpa ipsum eiusmod do adipisicing. Magna ex sint eu amet pariatur excepteur magna ullamco ut officia qui nostrud."
+    },
+    {
+      "username": "Lorrie",
+      "city": "Davenport",
+      "email": "<TypeError: undefined is not a function>",
+      "content": "Mollit proident amet aliquip eu voluptate nulla cillum veniam. Dolor exercitation cupidatat officia eiusmod adipisicing enim. Deserunt sit proident ipsum qui Lorem tempor. Nisi eiusmod veniam esse adipisicing voluptate nulla occaecat eiusmod ad aute consectetur quis sit. Esse ullamco sint culpa id."
+    },
+    {
+      "username": "Heath",
+      "city": "Finzel",
+      "email": "<TypeError: undefined is not a function>",
+      "content": "Pariatur pariatur aute sunt cillum aute Lorem nostrud adipisicing sit nostrud ad id. Eiusmod exercitation tempor eiusmod labore id consequat fugiat non amet Lorem. Aliquip amet laboris incididunt reprehenderit. Minim laborum id sint amet. Adipisicing ea aliquip tempor esse."
+    },
+    {
+      "username": "Nielsen",
+      "city": "Foscoe",
+      "email": "<TypeError: undefined is not a function>",
+      "content": "Eu duis nostrud culpa nisi non ipsum aute do officia eu eu ad do mollit. Ad aliquip reprehenderit consequat Lorem est. Elit officia cillum cillum ipsum aliquip. Aliqua sunt veniam deserunt exercitation aute elit cupidatat labore elit ut. Et nisi occaecat est aliquip labore dolore tempor amet ullamco nulla laborum consectetur excepteur."
+    }
+  ];
+
+}
--- README.md
+++ README.md
@@ -3,10 +3,15 @@
 
 ## Apprentissage d'Angular JS avec [Grafikart](Grafikart)
 
-- **Exercice 1 :** Les directives
+- **Exercice 1** : Les directives
 
   - **ng-model** : donne une valeur à une variable via un input / select etc.
   - **ng-repeat** : permet de boucler sur une balise
   - **filter** : permet de filtrer une variable, avec un pipe et un paramètre
     - *uppercase / lowercase* : changer la casse de la variable
     - *filter* : ne séléctionner qu'une chaine de caractère
+
+- **Exerice 2** : Controller et Scope
+
+  - **scope** : liaison entre le controller et la vue
+  - extension Chrome Angular Batarang pour inspecter les éléments d'Angular
