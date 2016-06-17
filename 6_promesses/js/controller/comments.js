725448c07edfe452d6afc2bed7d9a6af8a09d117
jvenerosy <julien.venerosy@gmail.com>
2016-06-17 20:20:57 +0200

    promesse et structuration ok

---
+++ 6_promesses/js/controller/comments.js
@@ -0,0 +1,26 @@
+app.controller('CommentsCtrl', function($scope, PostFactory, $routeParams) {
+
+  $scope.loading = true;
+  $scope.newComment = {};
+
+  PostFactory.getPost($routeParams.id).then(function(post) {
+    $scope.loading = false;
+    $scope.title = post.name;
+    $scope.comments = post.comments;
+  }, function(msg){
+    alert(msg);
+  })
+
+  $scope.addComment = function() {
+    $scope.comments.push($scope.newComment);
+
+    Post.add($scope.newComment).then(function(){
+
+    }, function(){
+      alert("Votre message n'a pas pu être envoyé");
+    })
+
+    $scope.newComment = {};
+  }
+
+});
